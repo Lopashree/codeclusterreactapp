a914b27666df86a4a9d3df5238369007ef992d06
Lopashree <138803894+Lopashree@users.noreply.github.com>
2023-08-01 20:25:17 +0530

    Initiall Commit

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Navbar from "./components/navbar/Navbar"
+import Footer from "./components/footer/Footer"
+import { AllRoutes } from './routes/allroutes';
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <AllRoutes/>
+      <Footer/>
+      </div>
+   )
+}
+
+export default App;
